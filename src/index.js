aad4125a8b5330c16d11d4009f4ba1b9c9b8bbf5
Samiksha Wagaj <samikshawagaj2003@gmail.com>
2025-01-28 12:15:47 +0530

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>hello everyone </h1>);
+root.render(<h1>hello world... </h1>);
