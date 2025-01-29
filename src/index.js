63c5b554582056f21e42f2c8cf272cbd0339c717
Samiksha Wagaj <samikshawagaj2003@gmail.com>
2025-01-29 16:11:24 +0530

    page modified

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import './BookCard.css';
 import BookCard, { BookCard2 } from "./BookCard.js";
 import image2 from "./cprogram-book.jpg";
 import image from "./datastructure-book.jpg";
@@ -8,7 +9,7 @@ import image from "./datastructure-book.jpg";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <>
+    <div className="card">
     <BookCard image={image}
     title="Data Structure" 
     price="₹749" 
@@ -19,7 +20,7 @@ root.render(
     price="₹250" 
     author=" Author :Vijitha Robinson "
     button="Buy Now"  />
-    </>
+    </div>
 )
 
 
