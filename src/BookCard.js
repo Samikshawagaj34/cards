37cf2b0c03e794bb83802719e80a392477adf4b9
Samiksha Wagaj <samikshawagaj2003@gmail.com>
2025-01-29 15:43:50 +0530

    inital commit

---
+++ src/BookCard.js
@@ -0,0 +1,35 @@
+ import React from "react";
+import './BookCard.css';
+
+ export default function BookCard(props ) {
+    console.log(props);
+    const { image,title,price, author,button } = props;
+    return(
+        <div className="card">
+        <div className="book-card">
+            <img src={image} className="book-image" alt="book" />
+            <h2 className="book-title">{title}</h2>
+            <p className="book-price">{price}</p>
+            <p className="book-author">{author}</p>
+            <button className="btn">{button}</button>
+        </div>
+        </div>
+    )
+ }
+
+ export function BookCard2( props ) {
+    const { image2,title,price, author,button } = props;
+    return(
+        <div className="card">
+        <div className="book-card">
+            <img src={image2} className="book-image" alt="book" />
+            <h2 className="book-title">{title}</h2>
+            <p className="book-price">{price}</p>
+            <p className="book-author">{author}</p>
+            <button className="btn">{button}</button>
+        </div>
+        </div>
+        
+    )
+    
+    }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import { createRoot } from "react-dom/client";
+import BookCard, { BookCard2 } from "./BookCard.js";
+import image2 from "./cprogram-book.jpg";
+import image from "./datastructure-book.jpg";
+
+
+
+
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>hello world... </h1>);
+root.render(
+    <>
+    <BookCard image={image}
+    title="Data Structure" 
+    price="₹749" 
+    author=" Author :Narasimha Karumanchi"
+    button="Buy Now"  /><br/>
+    <BookCard2  image2={image2} 
+    title="c Programming" 
+    price="₹250" 
+    author=" Author :Vijitha Robinson "
+    button="Buy Now"  />
+    </>
+)
+
+
+
+
+
